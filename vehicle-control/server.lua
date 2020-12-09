9f97c34bcff2f3eba1e05ebd1370bcfe722954b1
TrevorBarns <62955754+TrevorBarns@users.noreply.github.com>
2020-12-09 13:09:12 -0600

    Major changes
    Removed engine command (other script handles)
    Added /door /fdoor /rdoor to control doors

--- vehicle-control/server.lua
+++ vehicle-control/server.lua
@@ -1,71 +1,43 @@
--- E N G I N E --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/engine off" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('engineoff', s)
-	elseif message == "/engine on" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('engineon', s)
-	elseif message == "/engine" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('engine', s)
-	end
-end)
 -- T R U N K --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/trunk" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('trunk', s)
+RegisterCommand('trunk', function(source, args, user)
+	TriggerClientEvent('trunk', source)
+end, false)
+
+-- A L L  D O O R S --
+RegisterCommand('door', function(source, args, user)
+	if args[1] ~= nil then
+		local door = tonumber(args[1])
+		if door < 4 and door > -1 then
+			TriggerClientEvent('ChangeDoor', source, (door-1))	
+		end
+	else
+		TriggerClientEvent('ChangeDoor', source, 0)		
 	end
-end)
+end, false)
+
 -- R E A R  D O O R S --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/rdoors" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('rdoors', s)
+RegisterCommand('fdoor', function(source, args, user)
+	if args[1] ~= nil then
+		if args[1] == "p" or args[1] == "2" then
+			TriggerClientEvent('ChangeDoor', source, 1)
+		else 
+			TriggerClientEvent('ChangeDoor', source, 0)	
+		end
+	else
+			TriggerClientEvent('ChangeDoor', source, 0)		
 	end
-end)
+end, false)
+
+-- R E A R  D O O R S --
+RegisterCommand('rdoor', function(source, args, user)
+	if args[1] == "p" or args[1] == "2" then
+		TriggerClientEvent('ChangeDoor', source, 3)
+	else 
+		TriggerClientEvent('ChangeDoor', source, 2)
+	end
+end, false)
+
 -- H O O D --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/hood" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('hood', s)
-	end
-end)
--- L O C K --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/lock" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('lock', s)
-	end
-end)
--- S A V E --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/save" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('save', s)
-	end
-end)
--- R E M O T E --
-AddEventHandler('chatMessage', function(s, n, m)
-	local message = string.lower(m)
-	if message == "/sveh" then
-		CancelEvent()
-		--------------
-		TriggerClientEvent('controlsave', s)
-	end
-end)
+RegisterCommand('hood', function(source, args, user)
+	TriggerClientEvent('hood', source)
+end, false)
